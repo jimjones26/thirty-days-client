5451ff95c809948eaead9029fa8f6d53dd9ccc14
James Jones <jjones@Jamess-MacBook-Pro.local>
2020-05-19 22:07:00 -0400

    remove blog routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,6 @@
 import { Router } from "@beyonk/sapper-rbac";
 
 const routes = new Router()
-  .restrict("/about.*", ["admin"])
-  //.restrict("/blog.*", ["admin"])
   .restrict("/admin.*", ["admin"])
   .restrict("/organisation.*", ["owner"])
   .restrict("/profile.*", ["admin", "owner", "user"])
@@ -10,3 +8,15 @@ const routes = new Router()
   .build();
 
 export default routes;
+
+/* 
+LOGIN IS THE ONLY OPEN ROUTE
+if a route is restricted, a user must have one of the included scopes to access it
+if they do not have that scope, they will be redirected, in this case to the check auth server route
+if they have a valid access token, it means they are trying to hit a route they are not authorized for
+    - redirect back to referring route
+if they dont have access token, but do have a refresh token, they need to get new tokens
+    - redirect back to referring route
+if they have neither access or refresh token, they need to be redirected to login
+    - redirect to login
+*/
